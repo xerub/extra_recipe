99f40e97c603a723c7150ae0a524c4433b59d21c
xerub <xerub@aurora>
2017-05-30 09:51:29 +0000

    cydia thx luca

--- extra_recipe/unjail.m
+++ extra_recipe/unjail.m
@@ -122,7 +122,7 @@ unjail2(uint64_t surfacevt)
         return ERR_UNSUPPORTED_YET; // TODO: remove after writing KPP bypass
     }
 
-    if (0) {
+    if (1) {
         struct utsname uts;
         uname(&uts);
 
@@ -144,37 +144,98 @@ unjail2(uint64_t surfacevt)
         v_mount = kread_uint64(rootfs_vnode + off);
         kwrite_uint32(v_mount + 0x71, v_flag);
     }
-
     {
-        char path[4096];
+        char path[256];
         uint32_t size = sizeof(path);
         _NSGetExecutablePath(path, &size);
-        char *pt = realpath(path, NULL);
-
-        pid_t pd = 0;
-        NSString *execpath = [[NSString stringWithUTF8String:pt] stringByDeletingLastPathComponent];
-
-        NSString *tar = [execpath stringByAppendingPathComponent:@"tar"];
-        NSString *bootstrap = [execpath stringByAppendingPathComponent:@"bootstrap.tar"];
-        NSString *launchctl = [execpath stringByAppendingPathComponent:@"launchctl"];
-        const char *jl;
-
-        chdir("/tmp/");
-
-        jl = "/tmp/tar";
-        copyfile([tar UTF8String], jl, 0, COPYFILE_ALL);
-        chmod(jl, 0755);
-        posix_spawn(&pd, jl, NULL, NULL, (char **)&(const char*[]){ jl, "--preserve-permissions", "--no-overwrite-dir", "-xvf", [bootstrap UTF8String], NULL }, NULL);
-        NSLog(@"pid = %x", pd);
-        waitpid(pd, NULL, 0);
-
-        jl = "/tmp/bin/launchctl";
-        copyfile([launchctl UTF8String], jl, 0, COPYFILE_ALL);
-        chmod(jl, 0755);
-        posix_spawn(&pd, jl, NULL, NULL, (char **)&(const char*[]){ jl, "load", "/tmp/Library/LaunchDaemons", NULL }, NULL);
-        NSLog(@"pid = %x", pd);
-        waitpid(pd, NULL, 0);
+        char* pt = realpath(path, 0);
+        
+        {
+            __block pid_t pd = 0;
+            NSString* execpath = [[NSString stringWithUTF8String:pt]  stringByDeletingLastPathComponent];
+            
+            
+            int f = open("/.installed_yaluX", O_RDONLY);
+            
+            if (f == -1) {
+                NSString* tar = [execpath stringByAppendingPathComponent:@"tar"];
+                NSString* bootstrap = [execpath stringByAppendingPathComponent:@"bootstrap.tar"];
+                const char* jl = [tar UTF8String];
+                
+                unlink("/bin/tar");
+                unlink("/bin/launchctl");
+                
+                copyfile(jl, "/bin/tar", 0, COPYFILE_ALL);
+                chmod("/bin/tar", 0777);
+                jl="/bin/tar"; //
+                
+                chdir("/");
+                
+                posix_spawn(&pd, jl, 0, 0, (char**)&(const char*[]){jl, "--preserve-permissions", "--no-overwrite-dir", "-xvf", [bootstrap UTF8String], NULL}, NULL);
+                NSLog(@"pid = %x", pd);
+                waitpid(pd, 0, 0);
+                
+                
+                NSString* jlaunchctl = [execpath stringByAppendingPathComponent:@"launchctl"];
+                jl = [jlaunchctl UTF8String];
+                
+                copyfile(jl, "/bin/launchctl", 0, COPYFILE_ALL);
+                chmod("/bin/launchctl", 0755);
+                
+                open("/.installed_yaluX", O_RDWR|O_CREAT);
+                open("/.cydia_no_stash",O_RDWR|O_CREAT);
+                
+                
+                system("echo '127.0.0.1 iphonesubmissions.apple.com' >> /etc/hosts");
+                system("echo '127.0.0.1 radarsubmissions.apple.com' >> /etc/hosts");
+                
+                system("/usr/bin/uicache");
+                
+                system("killall -SIGSTOP cfprefsd");
+                NSMutableDictionary* md = [[NSMutableDictionary alloc] initWithContentsOfFile:@"/var/mobile/Library/Preferences/com.apple.springboard.plist"];
+                
+                [md setObject:[NSNumber numberWithBool:YES] forKey:@"SBShowNonDefaultSystemApps"];
+                
+                [md writeToFile:@"/var/mobile/Library/Preferences/com.apple.springboard.plist" atomically:YES];
+                system("killall -9 cfprefsd");
+                
+            }
+            {
+                NSString* jlaunchctl = [execpath stringByAppendingPathComponent:@"reload"];
+                char* jl = [jlaunchctl UTF8String];
+                unlink("/usr/libexec/reload");
+                copyfile(jl, "/usr/libexec/reload", 0, COPYFILE_ALL);
+                chmod("/usr/libexec/reload", 0755);
+                chown("/usr/libexec/reload", 0, 0);
+                
+            }
+            {
+                NSString* jlaunchctl = [execpath stringByAppendingPathComponent:@"0.reload.plist"];
+                char* jl = [jlaunchctl UTF8String];
+                unlink("/Library/LaunchDaemons/0.reload.plist");
+                copyfile(jl, "/Library/LaunchDaemons/0.reload.plist", 0, COPYFILE_ALL);
+                chmod("/Library/LaunchDaemons/0.reload.plist", 0644);
+                chown("/Library/LaunchDaemons/0.reload.plist", 0, 0);
+            }
+            {
+                NSString* jlaunchctl = [execpath stringByAppendingPathComponent:@"dropbear.plist"];
+                char* jl = [jlaunchctl UTF8String];
+                unlink("/Library/LaunchDaemons/dropbear.plist");
+                copyfile(jl, "/Library/LaunchDaemons/dropbear.plist", 0, COPYFILE_ALL);
+                chmod("/Library/LaunchDaemons/dropbear.plist", 0644);
+                chown("/Library/LaunchDaemons/dropbear.plist", 0, 0);
+            }
+            unlink("/System/Library/LaunchDaemons/com.apple.mobile.softwareupdated.plist");
+            
+        }
     }
+    chmod("/private", 0777);
+    chmod("/private/var", 0777);
+    chmod("/private/var/mobile", 0777);
+    chmod("/private/var/mobile/Library", 0777);
+    chmod("/private/var/mobile/Library/Preferences", 0777);
+    system("rm -rf /var/MobileAsset/Assets/com_apple_MobileAsset_SoftwareUpdate; touch /var/MobileAsset/Assets/com_apple_MobileAsset_SoftwareUpdate; chmod 000 /var/MobileAsset/Assets/com_apple_MobileAsset_SoftwareUpdate; chown 0:0 /var/MobileAsset/Assets/com_apple_MobileAsset_SoftwareUpdate");
+    system("(echo 'really jailbroken'; /bin/launchctl load /Library/LaunchDaemons/0.reload.plist)&");
 
     dlclose(h);
     return 0;
